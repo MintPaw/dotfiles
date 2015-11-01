99f7af71419704c4491800f6b5852a196b024951
MintPaw <jerusanders@gmail.com>
2015-11-01 08:07:23 -0700

    Improved strap_haxe

--- strap_haxe.sh
+++ strap_haxe.sh
@@ -1,5 +1,36 @@
-haxelib install openfl
-haxelib install lime
-haxelib install format
-haxelib install hxcpp
-haxelib run openfl setup
+mkdir -p ~/tools/haxe/lib
+mkdir -p ~/tools/haxe/libgit
+
+libs=("openfl" "lime" "format" "hxcpp" "flixel" 
+	"flixel-addons" "flixel-ui" "nape")
+
+libPaths=("https://github.com/openfl/openfl.git"
+	"https://github.com/openfl/lime.git"
+	"https://github.com/HaxeFoundation/format.git"
+	"https://github.com/HaxeFoundation/hxcpp.git"
+	"https://github.com/HaxeFlixel/flixel.git"
+	"https://github.com/HaxeFlixel/flixel-addons.git"
+	"https://github.com/HaxeFlixel/flixel-ui.git"
+	"https://github.com/deltaluca/nape.git")
+
+for ((i=0; i<${#libs[@]}; i++))
+do
+	echo ${libs[i]}
+
+	if [ ! -d ~/tools/haxe/libgit/${libs[i]} ]
+	then
+		git clone --recursive ${libPaths[i]} ~/tools/haxe/libgit/${libs[i]}
+	else
+		(cd ~/tools/haxe/libgit/${libs[i]}; git pull --recurse-submodules)
+	fi
+	
+	haxelib dev ${libs[i]} ~/tools/haxe/libgit/${libs[i]}
+done
+
+echo "Rebuilding."
+(
+	cd ~/tools/haxe/libgit/hxcpp/tools/hxcpp;
+	haxe compile.hxml;
+	cd ../../project; 
+	neko build.n -DHXCPP_M64
+)
