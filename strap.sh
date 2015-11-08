654638ec79e4a73922075d50f3f768bcab7935b3
MintPaw <jerusanders@gmail.com>
2015-11-08 10:01:52 -0700

    removed submodules

--- strap.sh
+++ strap.sh
@@ -1,1 +1,38 @@
 rsync -av  --exclude='.git*' --exclude='strap*' ~/dotfiles/ ~
+
+paths=(".oh-my-zsh"
+	".vim/bundle/vaxe"
+	".vim/bundle/ctrlp"
+	".vim/bundle/nerdtree"
+	"tools/imgur-screenshot"
+	".vim/bundle/gundo"
+	".vim/bundle/vitality"
+	".vim/bundle/unicode"
+	".vim/bundle/vim-multiple-cursors"
+	".vim/bundle/selection"
+	".vim/bundle/crunch")
+	
+gitUrls=("https://github.com/robbyrussell/oh-my-zsh.git"
+	"https://github.com/kien/ctrlp.vim.git"
+	"https://github.com/jdonaldson/vaxe.git"
+	"https://github.com/scrooloose/nerdtree.git"
+	"https://github.com/jomo/imgur-screenshot"
+	"https://github.com/sjl/gundo.vim.git"
+	"https://github.com/sjl/vitality.vim.git"
+	"https://github.com/chrisbra/unicode.vim.git"
+	"https://github.com/terryma/vim-multiple-cursors.git"
+	"https://github.com/arecarn/selection.vim.git"
+	"https://github.com/arecarn/crunch.vim.git")
+
+for ((i=0; i<${#paths[@]}; i++))
+do
+	echo ${paths[i]}
+
+	if [ ! -d ~/tools/haxe/libgit/${paths[i]} ]
+	then
+		git clone --recursive ${gitUrls[i]} ~/${paths[i]}
+	else
+		(cd ~/${paths[i]}; git pull --recurse-submodules)
+	fi
+	
+done
