8e61a55a03320b9148d9f2d759c2d14bc0eb381d
MintPaw <jerusanders@gmail.com>
2015-11-30 14:49:57 -0700

    Permissions to mount

--- sh/mountTcc.sh
+++ sh/mountTcc.sh
@@ -1,1 +1,2 @@
-sudo mount -t cifs //userhost.nmt.edu/accounts/j/jsande00 tcc -o username=jsande00
+sudo mount -t cifs //userhost.nmt.edu/accounts/j/jsande00 tcc -o username=jsande00,uid=1000,gid=1000
+sudo mount -t cifs //userhost.nmt.edu/accounts/m/mmcbride marciTcc -o username=jsande00,uid=1000,gid=1000
