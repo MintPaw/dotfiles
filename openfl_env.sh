c35dd6de7beaa97a9f5f73da1aca3f91f9786328
MintPaw <jerusanders@gmail.com>
2016-01-01 08:15:25 -0700

    General update

---
+++ openfl_env.sh
@@ -0,0 +1,27 @@
+if [ "$build" = "" ]; then
+	build=cpp
+fi
+
+function delete_openfl_exec()
+{
+	find bin/linux64/cpp/bin -type f | xargs file | grep 'ELF.*executable' | awk -F: '{print $1}' | xargs rm -f
+}
+
+if [ "$build" = "flash" ]; then
+	echo "flash"
+	function b() {rm -f bin/flash/bin/*.swf; openfl build flash -debug}
+	function r() {google-chrome-beta bin/flash/bin/test.html}
+	function bb() {b; r}
+elif [ "$build" = "cpp" ]; then
+	echo "cpp"
+	function b() {delete_openfl_exec; openfl build linux -debug}
+	function r() {cd bin/linux64/cpp/bin; find . -type f | xargs file | grep 'ELF.*executable' | awk -F: '{print $1}' | while read line; do; eval $line; done; cd -}
+	function bb() {b; r}
+elif [ "$build" = "html5" ]; then
+	echo "html5"
+	function b() {rm -f bin/html5/bin/index.html; openfl build html5 -debug}
+	function r() {(google-chrome-beta bin/html5/bin/index.html)}
+	function bb() {b; r}
+else
+	echo "No build"
+fi
